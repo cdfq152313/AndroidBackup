0f3214db250d42c0a2459b6a5f7c7530a1abfc9e
DennyCheng <denny.cheng@addweup.com>
2017-02-21 17:23:48 +0800

    HAHA

--- MyEncrypter/app/src/main/java/com/addweup/myencrypter/MainActivity.java
+++ MyEncrypter/app/src/main/java/com/addweup/myencrypter/MainActivity.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -16,6 +20,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 public class MainActivity extends AppCompatActivity {
     TextView result;
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     TextView display2;
     TextView display3;
 
+    final String IvAES = "1234567890abcdef";
+    AlgorithmParameterSpec mAlgorithmParameterSpec;
     SecretKey secretKey;
     Cipher cipher;
 
@@ -36,33 +43,48 @@ public class MainActivity extends AppCompatActivity {
         result = (TextView) findViewById(R.id.result);
     }
 
-    private void genAES() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+    private void genAES() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, UnsupportedEncodingException {
         KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(256); // for example
+        keyGen.init(256, new SecureRandom()); // for example
         secretKey = keyGen.generateKey();
+        mAlgorithmParameterSpec = new IvParameterSpec(IvAES.getBytes("UTF-8"));
     }
 
-    private String encrypt(String content) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+    private String encrypt(String content) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException {
         // Encode Content in base64
         byte [] contentB64 = Base64.encode(content.getBytes(), Base64.DEFAULT);
         // encrypt
         cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, mAlgorithmParameterSpec);
         byte [] encrypt = cipher.doFinal(contentB64);
-        String encryptStr = Base64.encodeToString(encrypt, Base64.NO_WRAP);
+        String encryptStr = Base64.encodeToString(encrypt, Base64.DEFAULT);
         return encryptStr;
     }
 
-    private String decrypt(String content) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+    private String decrypt(String content) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
         // Decode Base64
         byte [] contentB64 = Base64.decode(content, Base64.DEFAULT);
         // decrypt AES
         cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, mAlgorithmParameterSpec);
         byte [] decodeBytes = cipher.doFinal(contentB64);
         decodeBytes = Base64.decode(decodeBytes, Base64.DEFAULT);
         String str = new String(decodeBytes);
         return str;
+    }
+
+    byte [] temp;
+    private void encrypt2(String content) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException {
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, mAlgorithmParameterSpec);
+        temp = cipher.doFinal( content.getBytes( Charset.forName("UTF-8")) );
+    }
+
+    private String decrypt2() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, mAlgorithmParameterSpec);
+        byte [] decodeBytes = cipher.doFinal(temp);
+        return new String(decodeBytes, Charset.forName("UTF-8"));
     }
 
     public void genAES(View view){
