6e5e305ef0cf21e36dedf7b60afe1aaf247b3c3e
Denny Cheng <denny.cheng@addweup.com>
2017-05-25 15:44:08 +0800

    hoho

---
+++ MyEncrypter/app/src/main/java/com/addweup/myencrypter/AESTool.java
@@ -0,0 +1,71 @@
+package com.addweup.myencrypter;
+
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by denny on 2017/5/25.
+ */
+
+public class AESTool {
+
+    SecureRandom random = new SecureRandom();
+
+    public SecretKey genAESKey() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, UnsupportedEncodingException {
+        // AES key
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256, new SecureRandom());
+        return keyGen.generateKey();
+    }
+
+    public IvParameterSpec genInitializationVector() throws NoSuchPaddingException, NoSuchAlgorithmException {
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        byte [] iVAES = new byte[ cipher.getBlockSize() ];
+        random.nextBytes(iVAES);
+        return new IvParameterSpec(iVAES);
+    }
+
+    public String encrypt(String content, SecretKey secretKey, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+        byte [] temp = cipher.doFinal( content.getBytes() );
+        return Base64.encodeToString(temp, Base64.DEFAULT);
+    }
+
+    public String decrypt(String content, SecretKey secretKey, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        byte [] temp = Base64.decode(content, Base64.DEFAULT);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+        byte [] decodeBytes = cipher.doFinal(temp);
+        return new String(decodeBytes);
+    }
+
+    public String secretKeyToString(SecretKey secretKey){
+        return Base64.encodeToString(secretKey.getEncoded(), Base64.DEFAULT);
+    }
+
+    public String ivToString(IvParameterSpec iv){
+        return Base64.encodeToString(iv.getIV(), Base64.DEFAULT);
+    }
+
+    public SecretKey stringToSecretKey(String base64Key){
+        byte [] key = Base64.decode(base64Key, Base64.DEFAULT);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        return secretKeySpec;
+    }
+}
--- MyEncrypter/app/src/main/java/com/addweup/myencrypter/MainActivity.java
+++ MyEncrypter/app/src/main/java/com/addweup/myencrypter/MainActivity.java
@@ -6,7 +6,9 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.security.InvalidAlgorithmParameterException;
@@ -28,91 +30,35 @@ public class MainActivity extends AppCompatActivity {
     TextView display1;
     TextView display2;
     TextView display3;
+    TextView display4;
 
-    AlgorithmParameterSpec mAlgorithmParameterSpec;
+    AESTool aesTool;
+
     SecretKey secretKey;
-    Cipher cipher;
+    IvParameterSpec iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aesTool = new AESTool();
         display1 = (TextView) findViewById(R.id.display1);
         display2 = (TextView) findViewById(R.id.display2);
         display3 = (TextView) findViewById(R.id.display3);
+        display4 = (TextView) findViewById(R.id.display4);
         result = (TextView) findViewById(R.id.result);
-    }
-
-    private void genAESKey() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, UnsupportedEncodingException {
-        // AES key
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(256, new SecureRandom());
-        secretKey = keyGen.generateKey();
-    }
-
-    private void genInitializationVector() throws NoSuchPaddingException, NoSuchAlgorithmException {
-        SecureRandom random = new SecureRandom();
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        byte [] iVAES = new byte[ cipher.getBlockSize() ];
-        random.nextBytes(iVAES);
-        mAlgorithmParameterSpec = new IvParameterSpec(iVAES);
-    }
-
-    private String encrypt(String content) throws BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException {
-        // Encode Content in base64
-        byte [] contentB64 = Base64.encode(content.getBytes(), Base64.DEFAULT);
-        // encrypt
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, mAlgorithmParameterSpec);
-        byte [] encrypt = cipher.doFinal(contentB64);
-        String encryptStr = Base64.encodeToString(encrypt, Base64.DEFAULT);
-        return encryptStr;
-    }
-
-    private String decrypt(String content) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
-        // Decode Base64
-        byte [] contentB64 = Base64.decode(content, Base64.DEFAULT);
-        // decrypt AES
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, mAlgorithmParameterSpec);
-        byte [] decodeBytes = cipher.doFinal(contentB64);
-        decodeBytes = Base64.decode(decodeBytes, Base64.DEFAULT);
-        String str = new String(decodeBytes);
-        return str;
-    }
-
-    private String encrypt2(String content) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, mAlgorithmParameterSpec);
-        byte [] temp = cipher.doFinal( content.getBytes() );
-        return Base64.encodeToString(temp, Base64.DEFAULT);
-    }
-
-    private String decrypt2(String content) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        byte [] temp = Base64.decode(content, Base64.DEFAULT);
-        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, mAlgorithmParameterSpec);
-        byte [] decodeBytes = cipher.doFinal(temp);
-        return new String(decodeBytes);
-    }
-
-    private void export(){
-        String secretKeyB64 = Base64.encodeToString(secretKey.getEncoded(), Base64.DEFAULT);
-        IvParameterSpec iv = (IvParameterSpec) mAlgorithmParameterSpec;
-        String vectorB64 = Base64.encodeToString(iv.getIV(), Base64.DEFAULT);
-
-        Log.i("Secret Key Base64", secretKeyB64);
-        Log.i("Init Vector Base64", vectorB64);
     }
 
     public void initialize(View view){
         try {
             long start= System.currentTimeMillis();
-            genAESKey();
-            genInitializationVector();
+            secretKey = aesTool.genAESKey();
+            iv = aesTool.genInitializationVector();
             long end = System.currentTimeMillis();
             display1.setText("Milliseconds: " + (end-start) );
         } catch (Exception e) {
+            Toast.makeText(this, "Generate Key Failed", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
@@ -121,12 +67,13 @@ public class MainActivity extends AppCompatActivity {
         String word = result.getText().toString();
         try {
             long start= System.currentTimeMillis();
-            word = encrypt2(word);
+            word = aesTool.encrypt(word, secretKey, iv);
             long end = System.currentTimeMillis();
             display2.setText("Milliseconds: " + (end-start) );
             result.setText(word);
-            Log.i("Encrypt: ", word);
+            Log.i("Encrypt", word);
         } catch (Exception e) {
+            Toast.makeText(this, "Encrypt Failed", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
@@ -135,18 +82,31 @@ public class MainActivity extends AppCompatActivity {
         String word = result.getText().toString();
         try {
             long start= System.currentTimeMillis();
-            word = decrypt2(word);
+            word = aesTool.decrypt(word, secretKey, iv);
             long end = System.currentTimeMillis();
+
             display3.setText("Milliseconds: " + (end-start) );
             result.setText(word);
-            Log.i("Decrypt: ", word);
+            Log.i("Decrypt", word);
         } catch (Exception e) {
+            Toast.makeText(this, "Decrypt Failed", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
 
-    public void export(View view){
-        export();
+    public void exportKey(View view){
+        String secretKeyB64 = aesTool.secretKeyToString(secretKey);
+        String vectorB64 = aesTool.ivToString(iv);
+
+        display4.setText(secretKeyB64);
+        Log.i("Export", String.format("SecretKey B64: %s", secretKeyB64));
+        Log.i("Export", String.format("IV B64: %s", vectorB64));
+    }
+
+    public void importKey(View view){
+        String secretKeyB64 = display4.getText().toString();
+        secretKey = aesTool.stringToSecretKey(secretKeyB64);
+        Toast.makeText(this, "Read Successfully", Toast.LENGTH_SHORT).show();
     }
 }
 
